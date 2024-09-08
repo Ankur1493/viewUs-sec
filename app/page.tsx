3accb5812a61f996ec253e4ecf9c41081cf04a33
Ankur1493 <ankursharma1493@gmail.com>
2024-09-09 05:19:45 +0530

    comments added for UI changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col gap-5 justify-center items-center">
-      <Button>Click Me</Button>
-      <p className="font-bold text-6xl">Hi I am Ankur</p>
+      {/*create a good looking card or something which says collect reviews with view us*/}
     </div>
   );
 }
