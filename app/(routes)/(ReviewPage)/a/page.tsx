e7e04cc8f0e20940f716450570cba087f2fe5e1a
Ankur1493 <ankursharma1493@gmail.com>
2024-09-10 18:47:43 +0530

    page added for 'a' route

--- app/(routes)/(ReviewPage)/a/page.tsx
+++ app/(routes)/(ReviewPage)/a/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { ArrowUpRight, MessageSquare } from "lucide-react";
+
 export default function Page() {
   return (
-    <div>page</div>
+    <div>
+      <Card className="flex flex-col justify-center items-center bg-gradient-to-br from-red-400 via-red-300 to-pink-400 min-h-64">
+        <CardHeader>
+          <MessageSquare size={80} className="text-gray-800" />
+        </CardHeader>
+        <CardContent>
+          <p className="text-gray-800">Looks like you have entered a wrong URL</p>
+          <Link href="/" className="flex gap-4 text-4xl text-white hover:text-gray-200">Collect your own testimonials  with ViewUs.in <ArrowUpRight /> </Link >
+        </CardContent>
+      </Card>
+    </div>
   )
 }
