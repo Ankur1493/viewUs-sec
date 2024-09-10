beead97edb23081414244111ed2a623de9bb43e2
Ankur1493 <ankursharma1493@gmail.com>
2024-09-10 19:04:50 +0530

    eslint disabled for var db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,10 @@
+/* eslint-disable no-var */
 import { PrismaClient } from "@prisma/client"
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
+/* eslint-enable no-var */
 
 export const db = globalThis.prisma || new PrismaClient();
 
