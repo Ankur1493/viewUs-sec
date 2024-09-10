7636fe015eb8574b86b766ed366922bc9af3480f
Udit Kapoor <uditkapoor060@gmail.com>
2024-09-10 19:51:02 +0530

    Set up the zustand state

--- components/review/ReviewCard.tsx
+++ components/review/ReviewCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,45 +11,70 @@ import {
 } from "@/components/ui/card";
 import { Question } from "@prisma/client";
 import { ReviewForm } from "@/types";
+import useReviewPageStore from "@/store/useReviewPageStore";
+import TextReviewCard from "../textReview/TextReviewCard"; // Corrected import
+import VideoReviewCard from "../videoReview/VideoReviewCard";
 
-export default async function ReviewCard({
-  reviewForm,
-}: {
-  reviewForm: ReviewForm;
-}) {
+export default function ReviewCard({ reviewForm }: { reviewForm: ReviewForm }) {
+  const { clickedButton, setClickedButton } = useReviewPageStore();
+
   return (
-    <Card className="w-[450px] px-[2%]">
-      <CardHeader>
-        <div className="flex justify-center">
-          <Image
-            src={reviewForm.image!}
-            alt="logo"
-            height={80}
-            width={80}
-            className="rounded-full"
-          />
-        </div>
+    <>
+      {clickedButton === "Text" ? (
+        <TextReviewCard />
+      ) : clickedButton == "Video" ? (
+        <VideoReviewCard />
+      ) : (
+        <Card className="w-[450px] px-[2%]">
+          <CardHeader>
+            <div className="flex justify-center">
+              <Image
+                src={reviewForm.image!}
+                alt="logo"
+                height={80}
+                width={80}
+                className="rounded-full"
+              />
+            </div>
 
-        <CardTitle className="text-center text-[#33313B]">
-          {reviewForm.slug.toUpperCase()}
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="text-[#33313B] font-normal text-lg">
-          {reviewForm.heading}
-        </div>
-        <div className="mt-3">
-          <ul>
-            {reviewForm.questions.map((q: Question) => (
-              <li key={q.id}>Q. {q.question} ?</li>
-            ))}
-          </ul>
-        </div>
-      </CardContent>
-      <CardFooter className="flex flex-col gap-3">
-        <Button className="w-full">Record a Video</Button>
-        <Button className="w-full">Write a Testimonial</Button>
-      </CardFooter>
-    </Card>
+            <CardTitle className="text-center text-[#33313B]">
+              {reviewForm.slug.toUpperCase()}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-[#33313B] font-normal text-lg">
+              {reviewForm.heading}
+            </div>
+            <div className="mt-3">
+              <ul>
+                {reviewForm.questions.map((q: Question) => (
+                  <li key={q.id}>Q. {q.question} ?</li>
+                ))}
+              </ul>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col gap-3">
+            <Button
+              onClick={() => {
+                setClickedButton("Video");
+                console.log("Clicked Button: ", clickedButton);
+              }}
+              className="w-full"
+            >
+              Record a Video
+            </Button>
+            <Button
+              onClick={() => {
+                setClickedButton("Text");
+                console.log("Clicked Button: ", clickedButton);
+              }}
+              className="w-full"
+            >
+              Write a Testimonial
+            </Button>
+          </CardFooter>
+        </Card>
+      )}
+    </>
   );
 }
---
+++ components/videoReview/VideoReviewCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function VideoReviewCard() {
+  return <div>VideoReviewCard</div>;
+}
+
+export default VideoReviewCard;
