457b7f37a57f8a001d99b04c5f980b8949daadcc
Udit Kapoor <uditkapoor060@gmail.com>
2024-09-18 14:16:59 +0530

    solve bug

--- components/emailTemplates/text-review-submitted.tsx
+++ components/emailTemplates/text-review-submitted.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface EmailTemplateProps {
   firstName: string;
@@ -8,8 +8,10 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
 }) => (
   <div>
-    <h1>Now it's official!! we are fucking building this platform {firstName}</h1>
+    <h1>
+      Now its official!! we are fucking building this platform {firstName}
+    </h1>
   </div>
 );
-//1 create a mail to send a confirmation to user that review is stored, create this for normal 
-//2 add a email service call here,to send a mail to user notifying them that they have reviews more that 10 now, they need to update 
+//1 create a mail to send a confirmation to user that review is stored, create this for normal
+//2 add a email service call here,to send a mail to user notifying them that they have reviews more that 10 now, they need to update
