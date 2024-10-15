f44312ce3d112a7673b7a8254fcf243cf3f77b49
Ankur1493 <ankursharma1493@gmail.com>
2024-10-15 20:46:37 +0530

    db updated

--- models/review_model.ts
+++ models/review_model.ts
@@ -2,18 +2,23 @@ import { model, models, Schema } from "mongoose";
 
 enum ReviewType {
   TEXT = 0,
-  VIDEO = 1
+  VIDEO = 1,
+  IMPORTED = 3
 }
 
 export interface IReview {
   spaceId: string;
   reviewType?: ReviewType;
   review: string;
-  stars: Number;
-  name: string;
-  email: string;
-  designation?: string | null;
+  stars: number;
+  name: string | null;
+  email: string | null;
+  jobTitle?: string | null;
+  company?: string | null;
   image?: string | null;
+  importedImage?: string | null;
+  importedVideo?: string | null;
+  tags?: string[] | null;
 }
 
 const reviewSchema = new Schema<IReview>({
@@ -38,22 +43,53 @@ const reviewSchema = new Schema<IReview>({
   },
   name: {
     type: String,
-    required: true,
+    default: null
   },
   email: {
     type: String,
-    required: true,
+    default: null
   },
-  designation: {
+  jobTitle: {
     type: String,
-    default: null,
+    default: null
+  },
+  company: {
+    type: String,
+    default: null
   },
   image: {
     type: String,
     default: null,
+  },
+  importedImage: {
+    type: String,
+    default: null,
+    validate: {
+      validator: function (this: IReview) {
+        return this.reviewType === ReviewType.IMPORTED || !this.importedImage;
+      },
+      message: "Imported Image can only be set if reviewType is IMPORTED."
+    }
+  },
+  importedVideo: {
+    type: String,
+    default: null,
+    validate: {
+      validator: function (this: IReview) {
+        return this.reviewType === ReviewType.IMPORTED || !this.importedVideo;
+      },
+      message: "Imported Video can only be set if reviewType is IMPORTED."
+    }
+  }, tags: {
+    type: [String],
+    validate: {
+      validator: function (tags: string[]) {
+        return tags.length <= 3;  // Limit to 3 tags
+      },
+      message: "You can select up to 3 tags only."
+    }
   }
 });
 
 const Review = models.Review || model<IReview>("Review", reviewSchema);
 export default Review;
-
