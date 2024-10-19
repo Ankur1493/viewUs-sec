469bd3c26323ffb9c75fa57566872050bba2418a
Udit Kapoor <uditkapoor060@gmail.com>
2024-10-20 02:41:17 +0530

    merge conflicts

---
+++ app/(routes)/testing/profile/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Profile } from "@/components/extras/Profile";
+
+export default function Profle() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
