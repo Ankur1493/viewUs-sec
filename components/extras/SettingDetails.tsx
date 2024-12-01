88a4edfc423724f564da07a68e0bd4a52dfa4375
Ankur1493 <ankursharma1493@gmail.com>
2024-12-02 02:55:48 +0530

    settings details

--- components/extras/SettingDetails.tsx
+++ components/extras/SettingDetails.tsx
@@ -1,99 +1,61 @@
-"use client";
-
-import { useState } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { Video, MessageSquare, Zap, Import, BookHeart } from "lucide-react";
+import Link from "next/link";
 
 export default function SettingDetails() {
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="w-full flex items-top justify-center p-4 h-fit">
       <Card className="p-8 flex flex-col gap-4 w-full max-w-md shadow-lg hover:shadow-xl transition-shadow duration-300">
-        <CardHeader>
+        <CardHeader className="p-4">
           <CardTitle className="text-3xl font-bold text-center text-purple-700">
-            Your Current Plan
+            Subscribe to PRO for
           </CardTitle>
         </CardHeader>
         <CardContent className="flex flex-col gap-4">
           <div className="flex gap-4">
             <Card className="border-dashed border-purple-300 flex-1 hover:bg-purple-50 transition-colors duration-200">
               <CardHeader>
-                <CardTitle className="text-center font-medium text-lg flex flex-col items-center gap-2">
-                  <BookHeart className="text-purple-500" />1{" "}
-                  <span className="block text-sm text-gray-600">
-                    spaces left
-                  </span>
+                <CardTitle className="text-center p-0 font-medium text-lg flex flex-col items-center gap-2">
+                  <BookHeart className="text-purple-500" />3 Spaces{" "}
                 </CardTitle>
               </CardHeader>
             </Card>
-            <Card className="border-dashed border-pink-300 flex-1 hover:bg-pink-50 transition-colors duration-200">
-              <CardHeader>
+            <Card className="border-dashed border-pink-300  flex-1 hover:bg-pink-50 transition-colors duration-200">
+              <CardHeader className="p-0 px-2 py-6">
                 <CardTitle className="text-center font-medium text-lg flex flex-col items-center gap-2">
-                  <Import className="text-pink-500" />4{" "}
-                  <span className="block text-sm text-gray-600">
-                    imports left
-                  </span>
+                  <Import className="text-pink-500" />Infinte Imports
                 </CardTitle>
               </CardHeader>
             </Card>
           </div>
           <div className="flex gap-4">
-            <Card className="border-dashed border-pink-300 flex-1 hover:bg-pink-50 transition-colors duration-200">
-              <CardHeader>
-                <CardTitle className="text-center font-medium text-lg flex flex-col items-center gap-2">
-                  <Video className="text-pink-500" />2{" "}
-                  <span className="block text-sm text-gray-600">
-                    videos left
-                  </span>
-                </CardTitle>
-              </CardHeader>
-            </Card>
             <Card className="border-dashed border-purple-300 flex-1 hover:bg-purple-50 transition-colors duration-200">
               <CardHeader>
                 <CardTitle className="text-center font-medium text-lg flex flex-col items-center gap-2">
                   <MessageSquare className="text-purple-500" />
-                  12{" "}
-                  <span className="block text-sm text-gray-600">
-                    texts left
-                  </span>
+                  Infinite Texts
+                </CardTitle>
+              </CardHeader>
+            </Card>
+            <Card className="border-dashed border-pink-300 flex-1 hover:bg-pink-50 transition-colors duration-200">
+              <CardHeader>
+                <CardTitle className="text-center font-medium text-lg flex flex-col items-center gap-2">
+                  <Video className="text-pink-500" />7 Videos
                 </CardTitle>
               </CardHeader>
             </Card>
           </div>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div
-                  className="w-full"
-                  onMouseEnter={() => setIsHovered(true)}
-                  onMouseLeave={() => setIsHovered(false)}
-                >
-                  <Button
-                    variant="default"
-                    className={`w-full bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-300 transform ${
-                      isHovered ? "scale-105" : "scale-100"
-                    }`}
-                  >
-                    Upgrade{" "}
-                    <Zap
-                      className={`ml-2 ${isHovered ? "animate-pulse" : ""}`}
-                    />
-                  </Button>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Unlock more videos and unlimited texts and imports!</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+          <Link
+            href={"/pricing"}
+            className={`w-full flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-300 transform group"
+              }`}
+          >
+            Upgrade{" "}
+            <Zap
+              className="ml-2 group-hover:animate-pulse"
+            />
+          </Link>
         </CardContent>
       </Card>
     </div>
