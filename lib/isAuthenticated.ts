24c9521459246668342e80b856dd92cbaf67087a
Ankur1493 <ankursharma1493@gmail.com>
2024-12-04 17:18:09 +0530

    dynamic routed and suspense errors

---
+++ lib/isAuthenticated.ts
@@ -0,0 +1,11 @@
+//    const session = await auth();
+//    const user = session?.user;
+
+//    if (!user) {
+//      return NextResponse.json(
+//        { success: false, message: "login first", session, user },
+//        { status: 401 }
+//      );
+//   }
+
+
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
+import { NextRequest } from "next/server"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function getURLParams(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const slug = searchParams.get("slug");
+
+  if (!slug) {
+    return null
+  } else {
+    return slug
+  }
 }
