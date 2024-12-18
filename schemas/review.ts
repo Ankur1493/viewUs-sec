e9464e782e3b463beb8d7a241fffb2e0e76e3d93
Ankur1493 <ankursharma1493@gmail.com>
2024-12-18 12:00:18 +0530

    krde bhai bs

--- schemas/review.ts
+++ schemas/review.ts
@@ -2,7 +2,7 @@ import z from "zod"
 
 export const textReviewSchema = z.object({
   stars: z.number().min(1).max(5),
-  review: z.string().min(30, { message: "review should be atleast 20 characters long" }).max(500, { message: "review should be atleast 20 characters long" }),
+  review: z.string().min(30, { message: "review should be atleast 20 characters long" }).max(1000, { message: "review should be atleast 20 characters long" }),
   firstName: z.string().nullable().optional().default(null),
   lastName: z.string().nullable().optional().default(null),
   email: z.string().email(),
